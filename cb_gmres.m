8275135a5b76051bbbdd61bee30aeee0b572fa60
Thomas Grützmacher <thomas.gruetzmacher@kit.edu>
2022-02-24 03:35:03 +0100

    Finished 1st (potentially) working version

--- cb_gmres.m
+++ cb_gmres.m
@@ -52,91 +52,183 @@ n = size(A,1);
 vector_size = size(b);
 x = x_init;
 flag = false;
-residual = A*x - b;
+residual = b - A*x;
 residual_norm = norm(residual);
 b_norm = norm(b);
-relres = residual_norm/b_norm;
 
 iter = 0; % global iteration count
 local_iter = 0; % Iteration count since the last reset
 
 [residual_norm, residual_norm_collection, krylov_bases, next_krylov_basis] = initialize_2(residual, restart);
+relres = residual_norm/b_norm;
 
 hessenberg = zeros(restart + 1, restart);
 givens_sin = zeros(restart, 1);
 givens_cos = zeros(restart, 1);
 
-perform_reset = true;
-%perform_reset = false;
+%perform_reset = true;
+perform_reset = false;
 
 while (true)
-    if (iter == restart || perform_reset)
+    if (relres < tol)
+        flag = true;
+        break;
+    elseif (iter > maxit)
+        flag = false;
+        break;
+    end
+
+    if (local_iter == restart || perform_reset)
         % View not necessary because bounds are manually controlled
         %hessenberg_view = hessenberg(:, 1:local_iter);
-        before_precond = step_2(residual_norm_collection, krylov_bases, hessenberg, local_iter);
+        before_precond = ...
+            step_2(residual_norm_collection, krylov_bases, hessenberg, local_iter);
         x = x + M * before_precond;
         residual = b - A*x;
-        [residual_norm, residual_norm_collection, krylov_bases, next_krylov_basis] = initialize_2(residual, restart);
+        [residual_norm, residual_norm_collection, krylov_bases, ...
+            next_krylov_basis] = initialize_2(residual, restart);
         local_iter = 0;
     end
-    precond_vec = M * next_krylov_basis;
+    %precond_vec = M * next_krylov_basis;
+    %next_krylov_basis = A * precond_vec;
+    next_krylov_basis = A * M * next_krylov_basis;
     
     % finish_arnoldi_CGS
-    [next_krylov_basis, krylov_bases, hessenberg] = finish_arnoldi_CGS(next_krylov_basis, krylov_bases, hessenberg, local_iter);
+    [next_krylov_basis, krylov_bases, hessenberg] = ...
+        finish_arnoldi_CGS(next_krylov_basis, krylov_bases, hessenberg, local_iter);
     % givens_rotation
+    [givens_sin, givens_cos, hessenberg] = ...
+        givens_rotation(givens_sin, givens_cos, hessenberg, local_iter);
     % calculate_next_residual_norm
+    residual_norm_collection(local_iter+2) = ...
+        -givens_sin(local_iter+1) * residual_norm_collection(local_iter+1);
+    residual_norm_collection(local_iter+1) = ...
+        givens_cos(local_iter+1) * residual_norm_collection(local_iter+1);
+    residual_norm = abs(residual_norm_collection(local_iter+2));
+    relres = residual_norm / b_norm;
+
+%     print_matrix("Iteration:", local_iter)
+%     print_matrix("Hessenberg:", hessenberg)
+%     print_matrix("krylov_bases:", krylov_bases)
+%     print_matrix("next_krylov_basis:", next_krylov_basis)
+%     print_matrix("residual_norm:", residual_norm)
     %break
     iter = iter + 1;
     local_iter = local_iter + 1;
 end
+
+before_precond = ...
+    step_2(residual_norm_collection, krylov_bases, hessenberg, local_iter);
+x = x + M * before_precond;
+
 end
 
 
 function before_precond = step_2(residual_norm_collection, krylov_bases, hessenberg, local_iter)
-%TODO write in simpler terms!
 %solve_upper_triangular
-y = zeros(local_iter, 1);
-for i=local_iter:-1:1 % +1 since indices start at 1
-    tmp = residual_norm_collection(i);
-    for j=i+1:local_iter-1
-        tmp = tmp - hessenberg(i, j) * y(j);
-    end
-    y(i) = tmp / hessenberg(i, i);
-end
+y = hessenberg(1:local_iter, 1:local_iter) \ residual_norm_collection(1:local_iter);
 
 %calculate_qy
 before_precond = krylov_bases(:, 1:local_iter) * y;
-% before_precond = zeros(size(krylov_bases, 1), 1);
-% for i=1:size(before_precond,1)
-%     for j=1:local_iter
-%         before_precond(i) = before_precond(i) + krylov_bases(i, j) * y(j);
-%     end
-% end
 end
 
-function [residual_norm, residual_norm_collection, krylov_bases, next_krylov_basis] = initialize_2(residual, restart)
+
+function [residual_norm, residual_norm_collection, krylov_bases, next_krylov_basis] = ...
+    initialize_2(residual, restart)
 n = size(residual, 1);
 residual_norm = norm(residual);
 residual_norm_collection = zeros(restart + 1, 1);
 residual_norm_collection(1) = residual_norm;
 krylov_bases = zeros(n, restart+1);  % Transposed from Ginkgo storage
+krylov_bases(:, 1) = (1/residual_norm) *residual;
 next_krylov_basis = (1/residual_norm) * residual;
 end
 
-function [next_krylov_basis, krylov_bases, hessenberg] = finish_arnoldi_CGS(next_krylov_basis, krylov_bases, hessenberg, local_iter)
+
+function [next_krylov_basis, krylov_bases, hessenberg] = ...
+    finish_arnoldi_CGS(next_krylov_basis, krylov_bases, hessenberg, local_iter)
 eta = 1/sqrt(2);
 old_arnoldi_norm = eta * norm(next_krylov_basis);
-hessenberg_iter = zeros(local_iter + 1, 1);
+%hessenberg_iter = zeros(local_iter + 1, 1);
 hessenberg_iter = transpose(krylov_bases(:, 1:local_iter+1)) * next_krylov_basis;
 
+%print_matrix("next_krylov_basis before re-compute:", next_krylov_basis)
+%print_matrix("krylov_bases before:", krylov_bases)
+%print_matrix("hessenberg_iter before:", hessenberg_iter)
+
 next_krylov_basis = next_krylov_basis - krylov_bases(:, 1:local_iter+1) * hessenberg_iter;
+% for k = 1:local_iter+1
+%     for j=1:size(next_krylov_basis,1)
+%         next_krylov_basis(j) = next_krylov_basis(j) - hessenberg_iter(k) * krylov_bases(j, k);
+%     end
+% end
+
+%print_matrix("next_krylov_basis after re-compute:", next_krylov_basis)
 arnoldi_norm = norm(next_krylov_basis);
 
-% TODO add re-orthogonalization
+% Our re-orthogonalization in this loop
+for l=1:3
+    if (arnoldi_norm < old_arnoldi_norm)
+        break;
+    end
+    %print_matrix("re-orthogonalization", l)
+    old_arnoldi_norm = eta * arnoldi_norm;
+    %buffer = zeros(local_iter+1, 1);
+    buffer = transpose(krylov_bases(:, 1:local_iter+1)) * next_krylov_basis;
+    next_krylov_basis = next_krylov_basis - krylov_bases(:, 1:local_iter+1) * buffer;
+    hessenberg_iter = hessenberg_iter + buffer;
+    arnoldi_norm = norm(next_krylov_basis);
+end
 
-arnoldi_norm = norm(next_krylov_basis);
 hessenberg(1:local_iter+1, local_iter + 1) = hessenberg_iter;
 hessenberg(local_iter + 2, local_iter + 1) = arnoldi_norm;
 next_krylov_basis = (1/arnoldi_norm) * next_krylov_basis;
 krylov_bases(:, local_iter + 2) = next_krylov_basis;
+end
+
+
+function [givens_sin, givens_cos, hessenberg] = ...
+    givens_rotation(givens_sin, givens_cos, hessenberg, local_iter)
+hessenberg_iter = hessenberg(1:local_iter+2, local_iter+1);
+
+for j=1:local_iter
+    temp = givens_cos(j) * hessenberg_iter(j) + givens_sin(j) * hessenberg_iter(j+1);
+    hessenberg_iter(j+1) = -givens_sin(j) * hessenberg_iter(j) +...
+                          givens_cos(j) * hessenberg_iter(j+1);
+    hessenberg_iter(j) = temp;
+end
+
+%print_matrix("hessenberg mid givens_rotation:", hessenberg_iter)
+%print_matrix("givens_sin mid givens_rotation:", givens_sin)
+%print_matrix("givens_cos mid givens_rotation:", givens_cos)
+
+% calculate_sin_and_cos
+if (hessenberg_iter(local_iter+1) == 0)
+    givens_cos(local_iter+1) = 0;
+    givens_sin(local_iter+1) = 1;
+else
+    this_hess = hessenberg_iter(local_iter+1);
+    next_hess = hessenberg_iter(local_iter+2);
+    scale = abs(this_hess) + abs(next_hess);
+    hypotenuse = scale * sqrt(abs(this_hess / scale) * abs(this_hess / scale) +...
+        abs(next_hess / scale) * abs(next_hess / scale));
+    givens_cos(local_iter+1) = this_hess / hypotenuse;
+    givens_sin(local_iter+1) = next_hess / hypotenuse;
+end
+
+hessenberg_iter(local_iter+1) = ...
+    givens_cos(local_iter+1) * hessenberg_iter(local_iter+1) +...
+    givens_sin(local_iter+1) * hessenberg_iter(local_iter+2);
+hessenberg_iter(local_iter+2) = 0;
+
+%print_matrix("hessenberg end givens_rotation:", hessenberg_iter)
+%print_matrix("givens_sin end givens_rotation:", givens_sin)
+%print_matrix("givens_cos end givens_rotation:", givens_cos)
+
+hessenberg(1:local_iter+2, local_iter+1) = hessenberg_iter;
+end
+
+function print_matrix(str, mtx)
+    fprintf("%s\n", str)
+    disp(mtx)
 end
